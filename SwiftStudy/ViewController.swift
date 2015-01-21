9024b2fbfeb10fbcafb1a841ea31167eb976faf4
尹现伟 <yinxianwei@qq.com>
2015-01-21 17:01:07 +0800

    Initial Commit

---
+++ SwiftStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by 尹现伟 on 15-1-21.
+//  Copyright (c) 2015年 DNE Technology Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
