43c6800c7c4866274ee45333c16618ba4feccd96
尹现伟 <yinxianwei@qq.com>
2015-01-22 11:36:41 +0800

    lab计算

--- SwiftStudy/JEUtilityKit.swift
+++ SwiftStudy/JEUtilityKit.swift
@@ -14,11 +14,10 @@ import UIKit
 //FIXME: 这是一个bug
 
 //MARK: - 类似宏定义
-
 let ScreenHeight = UIScreen.mainScreen().bounds.size.height
 let ScreenWidth = UIScreen.mainScreen().bounds.size.width
 
-//MARK: - 类别（UIView）
+//MARK: - UIView
 extension UIView{
     func height()-> CGFloat{
         return self.frame.size.height;
@@ -27,22 +26,90 @@ extension UIView{
     func width()-> CGFloat{
         return self.frame.size.width;
     }
-    func BY()-> CGFloat{
-        return self.frame.size.height+self.frame.origin.y;
-    }
-
-    func BX()-> CGFloat{
-        return self.frame.size.width+self.frame.origin.x;
-    }
-    
-    func TY()-> CGFloat{
-        return self.frame.origin.y;
-    }
-    
-    func TX()-> CGFloat{
+    func xPos()-> CGFloat{
         return self.frame.origin.x;
     }
+    func yPos()-> CGFloat{
+        return self.frame.origin.y;
+    }
+}
 
+//MARK: - NSString
+extension NSString{
+
+    func stringSize(font:UIFont, lineModel:NSLineBreakMode, size:CGSize)-> CGSize{
+        if (self.length == 0) {
+            return ccs(0, 0)
+        }
+        var size_h:CGFloat = size.height
+        if(size.height<=0){
+            size_h = 10000
+        }
+        
+        var paragraphStyle:NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = lineModel
+        
+        var attribute:NSDictionary = [NSFontAttributeName:font,NSParagraphStyleAttributeName:paragraphStyle]
+        
+        var strSize:CGSize = self.boundingRectWithSize(ccs(size.width, size_h), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: attribute, context: nil).size
+        
+        return strSize
+    }
+}
+
+
+//MARK: - 坐标计算
+func VIEW_W(view:UIView)-> CGFloat{
+    return view.frame.size.width;
+}
+func VIEW_H(view:UIView)-> CGFloat{
+    return view.frame.size.height;
+}
+
+func VIEW_TX(view:UIView)-> CGFloat{
+    return view.frame.origin.x;
+}
+func VIEW_TY(view:UIView)-> CGFloat{
+    return view.frame.origin.y;
+}
+
+func VIEW_BX(view:UIView)-> CGFloat{
+    return view.frame.size.width+view.frame.origin.x;
+}
+func VIEW_BY(view:UIView)-> CGFloat{
+    return view.frame.size.height+view.frame.origin.y;
+}
+
+func FRAME_TX(frame:CGRect)-> CGFloat{
+    return frame.origin.x;
+}
+func FRAME_TY(frame:CGRect)-> CGFloat{
+    return frame.origin.y;
+}
+
+func FRAME_W(frame:CGRect)-> CGFloat{
+    return frame.size.width;
+}
+func FRAME_H(frame:CGRect)-> CGFloat{
+    return frame.size.height;
+}
+
+//MARK: - 方法简写
+func ccr(__X__:CGFloat,__Y__:CGFloat,__W__:CGFloat,__H__:CGFloat)-> CGRect{
+ return CGRectMake(__X__, __Y__, __W__, __H__)
+}
+
+func ccs(__W__:CGFloat,__H__:CGFloat)-> CGSize{
+    return CGSizeMake(__W__, __H__)
+}
+
+func ccp(__X__:CGFloat,__Y__:CGFloat)-> CGPoint{
+    return CGPointMake(__X__, __Y__)
+}
+
+func IMG(imageName:NSString)-> UIImage?{
+
+    return UIImage(named: imageName)
 }
 
 
--- SwiftStudy/ViewsController.swift
+++ SwiftStudy/ViewsController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewsController: UIViewController,UIAlertViewDelegate {
+class ViewsController: UIViewController,UIAlertViewDelegate,UIActionSheetDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +16,7 @@ class ViewsController: UIViewController,UIAlertViewDelegate {
         
         //创建个view
         var v:UIView = UIView(frame: CGRectMake(0, 0, ScreenWidth, ScreenHeight))
-        v.backgroundColor = UIColor.magentaColor()
+        v.backgroundColor = UIColor.lightGrayColor()
         self.view.addSubview(v)
         
         //创建个button
@@ -26,14 +26,64 @@ class ViewsController: UIViewController,UIAlertViewDelegate {
         button.backgroundColor = UIColor.blackColor()
         self.view.addSubview(button)
         
+        //创建个开关
+        var aSwitch:UISwitch = UISwitch(frame: ccr(VIEW_TX(button), VIEW_BY(button)+10, VIEW_W(button), 30))
+        aSwitch.addTarget(self, action: "switchChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        self.view.addSubview(aSwitch)
+        
+        //分段选择
+        var aSeg:UISegmentedControl = UISegmentedControl(frame: ccr(VIEW_TX(aSwitch), VIEW_BY(aSwitch)+20, 100, 30))
+        aSeg.insertSegmentWithTitle("第二", atIndex: 0, animated: true)
+        aSeg.insertSegmentWithTitle("第一", atIndex: 0, animated: true)
+        aSeg.selectedSegmentIndex = 0
+        aSeg.addTarget(self, action: "segSelectClick:", forControlEvents: UIControlEvents.ValueChanged)
+        self.view.addSubview(aSeg)
+        
+
+        var label:UILabel = UILabel(frame: ccr(VIEW_BX(aSeg)+10, VIEW_TY(button), 100, 30))
+        label.font = UIFont.systemFontOfSize(14)
+        label.text = "哈，哈哈哈，哈哈哈哈哈，shenmegui"
+        label.numberOfLines = 0
+        label.backgroundColor = UIColor.whiteColor()
+        label.lineBreakMode = NSLineBreakMode.ByCharWrapping
+        self.view.addSubview(label)
+        
+
+        var str:NSString = label.text!
+        
+
+        var size:CGSize = str.stringSize(label.font, lineModel: NSLineBreakMode.ByCharWrapping, size: ccs(VIEW_W(label), 0))
+
+        label.bounds = ccr(0, 0, size.width, size.height)
+
         
         
     }
+    
     
     func buttonClick(){
         
         println("哈哈哈哈")
         var alert:UIAlertView = UIAlertView(title: "提示", message: "点击了按钮", delegate: self, cancelButtonTitle: "取消", otherButtonTitles: "确定")
         alert.show()
+    }
+    
+    func switchChanged(swi:UISwitch){
+        
+        println("---->>\(swi.on)")
+        
+    }
+    
+    func segSelectClick(seg:UISegmentedControl){
+        if(seg.selectedSegmentIndex == 1){
+            var action:UIActionSheet = UIActionSheet(title: "选择", delegate: self, cancelButtonTitle: "取消", destructiveButtonTitle: "删除", otherButtonTitles: "更多")
+            action.showInView(self.view)
+            
+        }
+    }
+    
+//MARK: - UIActionSheetDelegate
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+        println("----\(buttonIndex)-----")
     }
 }
